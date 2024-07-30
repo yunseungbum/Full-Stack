dbd5c94ada9af5be7f5a1aa098bc691ff10c4bcc
yunseungbum <ibm15444@naver.com>
2024-07-30 17:43:48 +0900

    Add files via upload

---
+++ EX_0730_first/src/ex02_print/Ex04_variable.java
@@ -0,0 +1,13 @@
+package ex02_print;
+
+public class Ex04_variable {
+	public static void main(String[] args) {
+		//변수의 사용범위
+		//모든 변수는 자신이 만들어진 중괄호 안에서만 사용할 수 있다.
+		String favoriteFood;
+		favoriteFood = "돈까스";
+	}
+
+}
+
+
