4eab155f19619321e1e4f9ef0408e3d8553dad68
yunseungbum <ibm15444@naver.com>
2024-08-16 17:51:12 +0900

    Add files via upload

---
+++ Ex_0816/src/ex01_lamda/Ex03_lamda.java
@@ -0,0 +1,28 @@
+package ex01_lamda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class Ex03_lamda {
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<>(Arrays.asList("이름1","이름2","이름3","이름4","이름5"));
+		System.out.println(list.toString());
+		
+		for(String s : list) {
+			System.out.print(s+" ");
+		}
+		System.out.println();
+		
+		
+		//void accpt(T t)
+		// t -> System.out.prinfln(t);
+		//System.out::println
+		Consumer<String> print = t -> System.out.print(t+ " ");
+		list.forEach(print);
+		
+		
+	}
+
+}
